4cd08600837767a71fea8a47ee8793157de8addd
Relihazard <relihazard@gmail.com>
2017-09-15 00:29:23 +0200

    finishing part 1

--- includes/libft.h
+++ includes/libft.h
@@ -1,7 +1,9 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <ctype.h>
 # include <fcntl.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/types.h>
--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -1,5 +1,4 @@
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -8,14 +7,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	needle_len = ft_strlen(needle);
 	if (needle_len == 0 || len == 0)
 		return ((char *)haystack);
-	while (*haystack)
-		if (ft_memcmp(haystack++, needle, len) == 0)
+	while (*haystack && len--)
+		if (ft_memcmp(haystack++, needle, needle_len) == 0)
 			return ((char *)(haystack - 1));
 	return (NULL);
-}
-
-int		main(void)
-{
-	printf("%s", ft_strnstr("Foo Bar Baz", "Bar", 27));
-	return (0);
 }
