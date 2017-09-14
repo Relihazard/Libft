aff29e508e7705451a5cb0ac16119a12b395a450
Relihazard <relihazard@gmail.com>
2017-09-14 19:52:33 +0200

    norme

--- srcs/ft_memcmp.c
+++ srcs/ft_memcmp.c
@@ -3,7 +3,7 @@
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char	*s1_copy;
-	const unsigned char *s2_copy;
+	const unsigned char	*s2_copy;
 
 	s1_copy = (const unsigned char *)s1;
 	s2_copy = (const unsigned char *)s2;
