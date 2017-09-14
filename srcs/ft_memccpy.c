8112f7f7cf2e74d4ca8b235288e8cf9936168f0b
Relihazard <relihazard@gmail.com>
2017-09-14 14:53:22 +0200

    memccpy

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -5,10 +5,10 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	unsigned char		*dest;
 	const unsigned char	*source;
 
+	dest = (unsigned char *)dst;
+	source = (const unsigned char *)src;
 	if (dest && source)
 	{
-		dest = (unsigned char *)dst;
-		source = (const unsigned char *)src;
 		while (n != 0)
 		{
 			*dest++ = *source++;
