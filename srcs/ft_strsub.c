22d18470cf733e1853186145ea8ec6036eba89da
Relihazard <relihazard@gmail.com>
2017-10-03 21:37:48 +0200

    only four left

---
+++ srcs/ft_strsub.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+/*
+** Allocates and returns the copy of a stretch of the string s.
+** The stretch begins at the index start and has for length len.
+** If start and len don't designate a valid stretch from the string,
+** the behavior is undefined
+** \param s The string in which to search
+** \param start The index of the string where begin the stretch
+** \param len The length of the stretch to copy
+** \return The strech or NULL if the allocation failed
+*/
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*stretch;
+
+	if ((stretch = (char *)malloc(sizeof(*stretch) * (len + 1))) == NULL)
+		return (NULL);
+	while (start-- != 0)
+		s++;
+	ft_strncpy(stretch, s, len);
+	return (stretch);
+}
