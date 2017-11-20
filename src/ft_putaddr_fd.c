9d97b0a930ac2df9d531ebda0bcdd9cc5d2c009d
Axel Grossmann <agrossma@student.42.fr>
2017-11-20 16:42:53 +0100

    Create ft_putaddr_fd.c

---
+++ src/ft_putaddr_fd.c
@@ -0,0 +1,25 @@
+void  ft_putaddr_fd(void *ptr, int fd)
+{
+ long int   div;
+ char    tmp;
+ unsigned long int n;
+
+ n = (unsigned long int)ptr;
+ div = 16;
+ ft_putstr_fd("0x", fd);
+ while (n / div >= 16)
+  div *= 16;
+ while (div > 0)
+ {
+  tmp = '0' + n / div;
+  if (tmp > '9')
+  {
+   tmp += 39;
+   write(fd, &tmp, 1);
+  }
+  else
+   write(fd, &tmp, 1);
+  n %= div;
+  div /= 16;
+ }
+}
