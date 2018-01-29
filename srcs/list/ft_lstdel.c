6546474ea8b0f45eeb1e1b8b777e447dec9b9580
Axel Grossmann <agrossma@student.42.fr>
2018-01-29 16:41:38 +0100

    del

--- srcs/list/ft_lstdel.c
+++ srcs/list/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: agrossma <agrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 16:50:59 by agrossma          #+#    #+#             */
-/*   Updated: 2018/01/29 15:35:59 by agrossma         ###   ########.fr       */
+/*   Updated: 2018/01/29 16:40:40 by agrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,16 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
+	t_list	*tmp;
+
 	VOID_NULL_CHECK(alst);
 	VOID_NULL_CHECK(*alst);
+	tmp = NULL;
 	while (*alst)
 	{
+		tmp = (*alst)->next;
 		ft_lstdelone(alst, del);
-		*alst = (*alst)->next;
+		*alst = tmp;
 	}
+	ft_lstdelone(&tmp, del);
 }
--- srcs/list/ft_lstdelone.c
+++ srcs/list/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: agrossma <agrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 16:50:59 by agrossma          #+#    #+#             */
-/*   Updated: 2018/01/29 16:36:51 by agrossma         ###   ########.fr       */
+/*   Updated: 2018/01/29 16:41:26 by agrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 	VOID_NULL_CHECK(alst);
 	VOID_NULL_CHECK(*alst);
 	(*del)((*alst)->content, (*alst)->content_size);
-	*alst = NULL;
+	ft_memdel((void **)alst);
 }
